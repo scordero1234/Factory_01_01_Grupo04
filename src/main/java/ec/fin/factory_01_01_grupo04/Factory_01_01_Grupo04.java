eb59694c54313a8b044cacffb951262468c48bc8
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-09-19 16:48:53 -0500

    Initial commit

---
+++ src/main/java/ec/fin/factory_01_01_grupo04/Factory_01_01_Grupo04.java
@@ -0,0 +1,14 @@
+
+
+package ec.fin.factory_01_01_grupo04;
+
+/**
+ *
+ * @author grupo
+ */
+public class Factory_01_01_Grupo04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
