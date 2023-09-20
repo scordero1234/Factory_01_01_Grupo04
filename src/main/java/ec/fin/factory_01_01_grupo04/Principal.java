477f0581c7f0c542c6d95a428fd41311eb11fe8b
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-09-19 19:29:45 -0500

    calse Principal

--- src/main/java/ec/fin/factory_01_01_grupo04/Principal.java
+++ src/main/java/ec/fin/factory_01_01_grupo04/Principal.java
@@ -6,7 +6,7 @@ package ec.fin.factory_01_01_grupo04;
  *
  * @author grupo4
  */
-public class Factory_01_01_Grupo04 {
+public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
